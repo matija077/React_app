5b16268c36af545dba21c8ce3e26214f3a42c052
Matija077 <matija.prs@gmail.com>
2020-06-16 10:54:08 +0200

    more stuff done

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../components//directory/directory.component';
+
+function HomePage() {
+    return (
+        <div className="hoempage">
+            <Directory>
+
+            </Directory>
+        </div>
+    )
+}
+
+export default HomePage;
