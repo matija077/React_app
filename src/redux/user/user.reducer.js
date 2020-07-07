a87786aecd8f43d5927094061719218c94de6c01
Matija077 <matija.prs@gmail.com>
2020-07-07 11:02:33 +0200

    add redux, create reducers and redux folder
    -added redux, react-redux and loggign middleware
    -create redxu structure folder
    -created root reducer
    -created user reducer

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,19 @@
+var INITIAL_STATE = {
+    currentUser: null,
+};
+
+function userReducer(currentState = INITIAL_STATE, action) {
+    switch(action.type) {
+        case 'SET_CURRENT_USER': {
+            return {
+                ...currentState,
+                currentUser: action.payload,
+            };
+        }
+        default: {
+            return currentState;
+        }
+    };
+}
+
+export default userReducer;
