7782d8bec6a19e64ca705400bd058edf86324e41
Matija077 <matija.prs@gmail.com>
2020-07-27 15:53:18 +0200

    add cart item component

--- src/components/cart/cart.component.js
+++ src/components/cart/cart.component.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import SubmitComponent from '../SubmitButton/SubmitButton.component';
+import CartItem from '../cart-item/cartItem.component';
 
 import './cart.style.scss';
 
-function Cart() {
+function Cart({ cartItems }) {
+    console.log(cartItems);
     return(
         <div className="cart-dropdown">
             <div className="cart-items">
+                {
+                    cartItems.map(cartItem =>
+                        <CartItem
+                            key={cartItem.id}
+                            item={cartItem}>
+                        </CartItem>
+                    )
+                }
             </div>
             <SubmitComponent>GO TO CHECKOUT</SubmitComponent>
         </div>
     );
 }
 
-export default Cart;
+function mapStateToProps({ cart: { cartItems } }) {
+    return {
+        cartItems: cartItems
+    };
+}
+
+export default connect(mapStateToProps)(Cart);
