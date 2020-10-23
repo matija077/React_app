092043c70fae9d3058b134b46b15402297823c8a
Matija077 <matija.prs@gmail.com>
2020-10-23 16:53:46 +0200

    add clear cart component add clear cart into min icheckout add clear cart to checkout ame it re usable

---
+++ src/components/clear-cart/clearCart.styles.js
@@ -0,0 +1,8 @@
+import styled, { css } from 'styled-components';
+
+export const ClearCartItemsContainer = styled.div`
+    width: 10px;
+    height: 30px;
+    display: flex;
+    margin-left: auto;
+`;
