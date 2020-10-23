8f1ed93a8247cf11cc90ac76e31e80280b6fe11c
Matija077 <matija.prs@gmail.com>
2020-10-23 17:40:13 +0200

    change shop to use one export saga

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,12 +1,12 @@
 import { all, call } from 'redux-saga/effects';
 
-import { fetchCollectionStart } from './shop/shop.saga';
+import { shopSaga } from './shop/shop.saga';
 import userSaga from './user/user.saga';
 import cartSaga from './cart/cart.saga';
 
 export default function* rootSaga() {
     yield all([
-        call(fetchCollectionStart),
+        call(shopSaga),
         call(userSaga),
         call(cartSaga)
     ]);
