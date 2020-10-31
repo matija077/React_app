7851034a44d271ce81594f18d432cf279da391d9
Matija077 <matija.prs@gmail.com>
2020-10-31 11:29:45 +0100

    add stripe on backend
    add giignore to main folder

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 const PORT_CONST = 5000;
+const returnCodes = {
+    'error': 500,
+    'ok': 200,
+}
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
+
+var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 var app = express();
 var port = process.env.PORT || PORT_CONST;
@@ -31,4 +37,24 @@ app.listen(port,function whileRunning(error) {
     }
 
     console.log('Server runign on port ' + port);
+});
+
+app.post('/payment', function serve(req, res) {
+    var body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    };
+
+    stripe.charges.create(body,
+        function(stripeError, stripeResponse) {
+            if (stripeError) {
+                res.status(returnCodes.error).send(
+                    { error: stripeError }
+                );
+            } else {
+                res.status(returnCodes.ok).send(
+                    { success: stripeResponse })
+            }
+        });
 });
