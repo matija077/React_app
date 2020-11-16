00509e112959077c11e7ae5f61a80e5f91dbc4ff
Matija077 <matija.prs@gmail.com>
2020-11-16 16:21:31 +0100

    save testign for now

--- client/src/components/card-icon/card-icon.test.js
+++ client/src/components/card-icon/card-icon.test.js
@@ -8,18 +8,25 @@ import { shallow } from 'enzyme';
 import CartIcon from './card-icon.component';
 
 
-var mockToggleCartHidden = function() {
-    console.log("hidden");
-}
-var mockItemCount= 0;
-var cardIconComponent = <CartIcon
-    store={store}
-    toggleCartHidden={mockToggleCartHidden}
-    itemCount={mockItemCount}
-/>;
+var mockToggleCartHidden, mockItemCount,
+    cardIconComponent, shallowWrapper,
+    cardIconComponent;
 
-var shallowWrapper = shallow(cardIconComponent);
-var cardIconComponent = reactRender(cardIconComponent);
+beforeEach(function beforeEachFunction() {
+    mockToggleCartHidden = function() {
+        console.log("hidden");
+    }
+    mockItemCount = 0;
+    cardIconComponent = <CartIcon
+        store={store}
+        toggleCartHidden={mockToggleCartHidden}
+        itemCount={mockItemCount}
+    />;
+
+    shallowWrapper = shallow(cardIconComponent);
+    cardIconComponent = reactRender(cardIconComponent);
+
+});
 
 describe("test cardIcon component", function describe() {
     it("snapshot card icon enzyme", function snapshot(){
@@ -28,12 +35,16 @@ describe("test cardIcon component", function describe() {
     })
 
     it("card icon enzyme", function enzyme(){
-        //const cartIcon = shallowWrapper.find("[className='cart-icon']");
+        expect.assertions(0);
+        //const cartIcon = shallowWrapper.find("className: cart-icon");
         //expect(cartIcon.hasClass('cart-icon')).toEqual('cart-icon');
         //const shoppingIcon = wrapper.find("[className='shopping-icon']");
         //expect(shoppingIcon.hasClass('shopping-icon')).toEqual('shopping-icon');
         //const itemCount = wrapper.find("[className='item-count']");
         //expect(itemCount.hasClass('item-count')).toEqual('item-count');
+        /*console.log(shallowWrapper.props().toggleCartHidden);
+        expect(shallowWrapper.props().itemCount).toBe(0);
+        console.log(shallowWrapper.props().itemCount);*/
     })
 
     it("icon cart react DOM", function react(){
